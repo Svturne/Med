8a91fe3241b1b531847451f3506dae23c115be7d
Mehdi Badsi <mehdi.bad@outlook.com>
2023-02-19 18:00:09 +0100

    add two input in login

---
+++ assets/colors.js
@@ -0,0 +1,9 @@
+export default colors = {
+  blue: '#4470c1',
+  green: '#DFF4EF',
+  grey: '#6E6972',
+  blue2: '#7FA1D8',
+  brow: '#A18667',
+  white: '#FFFFFF',
+  black: '#000000',
+};
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,11 +1,29 @@
-import {Image, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {Image, StyleSheet, Text, TextInput, View} from 'react-native';
+import React, {useState} from 'react';
 import doctorPic from '../../assets/images/doctor.png';
+import colors from '../../assets/colors';
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <View>
+    <View style={styles.mainContainer}>
       <Image source={doctorPic} style={styles.image} />
-      <View style={styles.container}></View>
+      <View style={styles.container}>
+        <View style={{backgroundColor: colors.green, justifyContent: 'center'}}>
+          <TextInput
+            value={email}
+            onChangeText={setEmail}
+            style={{padding: 10}}
+          />
+        </View>
+        <View style={{backgroundColor: colors.green, justifyContent: 'center'}}>
+          <TextInput
+            value={password}
+            onChangeText={setPassword}
+            style={{padding: 10}}
+          />
+        </View>
+      </View>
     </View>
   );
 };
@@ -13,10 +31,16 @@ const Login = () => {
 export default Login;
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    height: '100%',
+    width: '100%',
+  },
   image: {
     width: '100%',
   },
   container: {
-    backgroundColor: 'red',
+    backgroundColor: colors.blue,
+    flex: 1,
+    justifyContent: 'space-around',
   },
 });
