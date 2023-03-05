012074f96612d7da849ca943b0a7441b2a0666e5
Svturne <mehdi.bad@outook.com>
2023-03-05 11:34:42 +0100

    finished navigate patient

--- src/screens/PatientScreen.js
+++ src/screens/PatientScreen.js
@@ -1,10 +1,10 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
-const PatientScreen = () => {
+const PatientScreen = ({route}) => {
   return (
     <View>
-      <Text style={{color: 'black'}}>PatientScreen ID:</Text>
+      <Text style={{color: 'black'}}>PatientScreen ID: {route.params.id}</Text>
     </View>
   );
 };
