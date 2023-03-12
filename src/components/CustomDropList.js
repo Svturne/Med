5fd9ec5b8aa9fcedc5699830f8eb7897ba9213a6
Svturne <mehdi.bad@outook.com>
2023-03-12 13:45:58 +0100

    finieshed create patient view

---
+++ src/components/CustomDropList.js
@@ -0,0 +1,32 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {SelectList} from 'react-native-dropdown-select-list';
+
+const CustomDropList = props => {
+  return (
+    <View>
+      <SelectList
+        setSelected={props.onSelect}
+        data={props.data}
+        save="value"
+        placeholder={props.title}
+        search={false}
+        boxStyles={{
+          backgroundColor: colors.green,
+          textColor: colors.black,
+          borderRadius: 10,
+          borderColor: colors.white,
+          marginVertical: 16,
+        }}
+        dropdownStyles={{backgroundColor: colors.green}}
+        dropdownTextStyles={{color: colors.black}}
+        fontFamily={fonts.regular}
+        inputStyles={{color: colors.black, padding: 5}}
+      />
+    </View>
+  );
+};
+
+export default CustomDropList;
+
+const styles = StyleSheet.create({});
--- src/screens/CreatePatient.js
+++ src/screens/CreatePatient.js
@@ -1,16 +1,55 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import colors from '../../assets/colors';
 import fonts from '../../assets/fonts/fonts';
 import CustomInput from '../components/CustomInput';
+import CustomDropList from '../components/CustomDropList';
+import CustomButton from '../components/CustomButton';
 
 const CreatePatient = () => {
+  const [selected, setSelected] = useState('');
+  const [createPatientLoader, setCreatePatientLoader] = useState(false);
+  const [fullname, setFullname] = useState('');
+  const [email, setEmail] = useState('');
+  const [age, setAge] = useState(0);
+
+  const data = [
+    {key: '1', value: 'Masculin'},
+    {key: '2', value: 'Féminin'},
+  ];
+
+  const create = useCallback(() => {
+    //TODO: send create patient request
+  }, [fullname, email, age, selected]);
+
   return (
     <View style={styles.mainContainer}>
       <Text style={styles.titleText}>Ajouter un nouveau patient</Text>
-      <CustomInput nameOfInpute="Nom" />
-      <CustomInput nameOfInpute="Email" />
+      <CustomInput
+        nameOfInpute="Nom"
+        value={fullname}
+        onChangeText={setFullname}
+        secure={false}
+      />
+      <CustomInput
+        nameOfInpute="Email"
+        value={email}
+        onChangeText={setEmail}
+        secure={false}
+      />
       <CustomInput nameOfInpute="Age" />
+
+      <CustomDropList
+        title="Sexe"
+        data={data}
+        onSelect={val => setSelected(val)}
+      />
+      <CustomButton
+        text="Enregistrer"
+        color="lightgreen"
+        isLoading={createPatientLoader}
+        func={create}
+      />
     </View>
   );
 };
@@ -23,10 +62,11 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     padding: 20,
+    justifyContent: 'center',
   },
   titleText: {
     fontFamily: fonts.semiBold,
-    fontSize: 20,
+    fontSize: 30,
     color: colors.white,
     textAlign: 'center',
   },
